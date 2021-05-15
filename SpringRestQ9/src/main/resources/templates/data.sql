a04fa7704f160156105961b1acf128ff215deff2
Anuragini <anuraginipathak0@gmail.com>
2021-05-15 16:13:02 +0530

    9

---
+++ SpringRestQ9/src/main/resources/templates/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO 
+	TBL_EMPLOYEES (first_name, last_name, email) 
+VALUES
+  	('Niharika', 'Bora', 'M G Marg', 'Sikkim', '700102', 'India'),
+  	('Ethan', 'Miller', 'Dispur', 'Guwahati', '781001', 'India');
