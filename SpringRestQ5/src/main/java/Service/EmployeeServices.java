e5b56924910734d0f8f3f9ddc0f423b1a9b4106d
Anuragini <anuraginipathak0@gmail.com>
2021-05-15 16:39:33 +0530

    5

---
+++ SpringRestQ5/src/main/java/Service/EmployeeServices.java
@@ -0,0 +1,37 @@
+package Service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import Entity.Employee;
+
+@Service
+public class EmployeeServices {
+
+	public List<Employee> getEmployee() {
+		
+		return null;
+	}
+
+	public Employee getEmployee(int id) {
+		
+		return null;
+	}
+
+	public List<Employee> addOneEmployee(Employee employee) {
+		
+		return null;
+	}
+
+	public List<Employee> updateOneEmployee(int id, Employee employee) {
+		
+		return null;
+	}
+
+	public List<Employee> deleteOneInventory(int id) {
+		
+		return null;
+	}
+
+}
